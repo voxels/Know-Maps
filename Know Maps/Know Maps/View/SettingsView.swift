05b0e96282b7aeaa4361b4ae0df39d20431a718c
voxels <voxels@gmail.com>
2023-11-29 11:25:43 -0500

    Adding app icon

--- Know Maps/Know Maps/View/SettingsView.swift
+++ Know Maps/Know Maps/View/SettingsView.swift
@@ -13,37 +13,40 @@ struct SettingsView: View {
 
     var body: some View {
         if model.userId.isEmpty {
-            SignInWithAppleButton { request in
-                if !model.userId.isEmpty {
-                    request.user = model.userId
-                }
-                request.requestedScopes = [.fullName]
-            } onCompletion: { result in
-                switch result {
-                case .success(let authResults):
-                    if let appleIDCredential = authResults.credential as? ASAuthorizationAppleIDCredential {
-                        model.userId = appleIDCredential.user
-                        model.fullName = "\(appleIDCredential.fullName?.givenName ?? "") \(appleIDCredential.fullName?.familyName ?? "")"
-                        print("Authorization successful.")
-
-                        Task {
-                            let key =  model.userId.data(using: .utf8)
-                            let addquery: [String: Any] = [kSecClass as String: kSecClassKey,
-                                                           kSecAttrApplicationTag as String: SettingsModel.tag,
-                                                           kSecValueData as String: key as AnyObject]
-                            let status = SecItemAdd(addquery as CFDictionary, nil)
-                            guard status == errSecSuccess else {
-                                print(status)
-                                return
-                            }
-                            print("Storing Apple ID successful.")
-                        }
+            ZStack() {
+                Rectangle().frame(width: <#T##CGFloat?#>)
+                SignInWithAppleButton { request in
+                    if !model.userId.isEmpty {
+                        request.user = model.userId
                     }
-                case .failure(let error):
-                    print("Authorization failed: " + error.localizedDescription)
+                    request.requestedScopes = [.fullName]
+                } onCompletion: { result in
+                    switch result {
+                    case .success(let authResults):
+                        if let appleIDCredential = authResults.credential as? ASAuthorizationAppleIDCredential {
+                            model.userId = appleIDCredential.user
+                            model.fullName = "\(appleIDCredential.fullName?.givenName ?? "") \(appleIDCredential.fullName?.familyName ?? "")"
+                            print("Authorization successful.")
+
+                            Task {
+                                let key =  model.userId.data(using: .utf8)
+                                let addquery: [String: Any] = [kSecClass as String: kSecClassKey,
+                                                               kSecAttrApplicationTag as String: SettingsModel.tag,
+                                                               kSecValueData as String: key as AnyObject]
+                                let status = SecItemAdd(addquery as CFDictionary, nil)
+                                guard status == errSecSuccess else {
+                                    print(status)
+                                    return
+                                }
+                                print("Storing Apple ID successful.")
+                            }
+                        }
+                    case .failure(let error):
+                        print("Authorization failed: " + error.localizedDescription)
+                    }
                 }
+                .signInWithAppleButtonStyle(.black)
             }
-            .signInWithAppleButtonStyle(.black)
         }
         else {
             if let fullName = model.fullName?.trimmingCharacters(in: .whitespaces) {
