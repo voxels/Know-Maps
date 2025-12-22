1538061eee1d6addf03abd65f59ff6a22c1adc07
voxels <voxels@gmail.com>
2025-12-22 07:31:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,19 @@ let package = Package(
             targets: ["knowmaps"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/kean/Nuke", exact: "12.8.0"),
+        .package(url: "https://github.com/segmentio/analytics-swift", exact: "1.6.2"),
+        .package(url: "https://github.com/supabase/supabase-swift", exact: "2.33.1"),
+    ],
     targets: [
         .target(
             name: "knowmaps",
+            dependencies: [
+                .product(name: "NukeUI", package: "Nuke"),
+                .product(name: "Segment", package: "analytics-swift"),
+                .product(name: "Supabase", package: "supabase-swift"),
+            ],
             path: "Know-Maps/knowmaps"
         ),
         .testTarget(
@@ -27,4 +37,3 @@ let package = Package(
         ),
     ]
 )
-
