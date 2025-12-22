667d989ffbd5f20d1a4028397408de01e40ef358
voxels <voxels@gmail.com>
2025-12-22 12:21:43 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "knowmaps",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
-        .visionOS(.v1),
+        .iOS(.v18),
+        .macOS(.v15),
+        .visionOS(.v2),
     ],
     products: [
         .library(name: "knowmaps", targets: ["knowmaps"]),
@@ -68,3 +68,4 @@ let package = Package(
         ),
     ]
 )
+
